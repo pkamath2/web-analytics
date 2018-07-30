0174391cc7cb2462f542a5f506edcb7b39528f4f
pkamath2 <git.pkamath2@gmail.com>
2018-07-30 14:18:22 +0800

    Added test

---
+++ src/test/java/org/pk/web_analytics/processor/WebAnalyticsProcessorTest.java
@@ -0,0 +1,20 @@
+package org.pk.web_analytics.processor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Author: purnimakamath
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebAnalyticsProcessorTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
